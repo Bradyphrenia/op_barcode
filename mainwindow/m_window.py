771643be7d5994177833f5e58b1b413d642c749c
steffen <stro889002@aol.com>
2025-06-11 15:52:13 +0200

    Add MainWindow implementation and update project SDK configuration

--- mainwindow/m_window.py
+++ mainwindow/m_window.py
@@ -1,6 +1,7 @@
 import sys
+from PyQt5 import QtWidgets as qtw
 from PyQt5.QtWidgets import QMainWindow
-from .mainwindow import Ui_MainWindow
+from mainwindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -8,9 +9,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.pushButton_ok.clicked.connect(self.close)
+        self.ui.pushButton_decode.clicked.connect(self.barcode_decode)
 
+    def barcode_read(self):
+        barcode = self.ui.lineEdit_barcode.text()
+        return barcode
 
+    def barcode_decode(self, barcode):
+        # barcode = '01042801021324431727113010Y018'
+        # barcode = barcode.decode('utf-8')
 
+        gtin = barcode[2:16]
+        expires = barcode[18:24]
+        serial = barcode[26:32]
+        print(gtin, expires, serial)
+        return gtin, expires, serial
 
 
 if __name__ == '__main__':
